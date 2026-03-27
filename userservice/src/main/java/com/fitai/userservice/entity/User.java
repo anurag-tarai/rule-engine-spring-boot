20520805ced658c1a31af3cbc4572367b51c5915
Anurag-Tarai <anuragtarai051@gmail.com>
2026-03-27 20:50:31 +0530

    feat(userservice): Databse schema & designed User entity

---
+++ userservice/src/main/java/com/fitai/userservice/entity/User.java
@@ -0,0 +1,36 @@
+package com.fitai.userservice.entity;
+
+import com.fitai.userservice.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ userservice/src/main/java/com/fitai/userservice/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.fitai.userservice.enums;
+
+public enum UserRole {
+    USER, ADMIN
+}
